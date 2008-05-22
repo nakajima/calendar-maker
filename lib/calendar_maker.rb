4b28e83ca905e63401b436cfdc540f6e0fa052ec
James Herdman <james.herdman@gmail.com>
2008-05-22 18:45:27 -0400

    Don't lift code when you can just require it. This is the sane approach. If you find that your library breaks with an upgrade to your requirement, use 'gem' to require a specific version.

--- lib/calendar_maker.rb
+++ lib/calendar_maker.rb
@@ -1,5 +1,12 @@
 $:.unshift(File.expand_path(File.dirname(__FILE__)))
 
+begin
+  require 'facets'
+rescue
+  require 'rubygems'
+  require 'facets'
+end
+
 require 'date'
 require 'calendar_maker/string_extensions'
 require 'calendar_maker/view_helpers'
--- lib/calendar_maker/string_extensions.rb
+++ lib/calendar_maker/string_extensions.rb
@@ -1,35 +1,5 @@
-module StringHelpers
-  
-  # Borrowed from Facets gem
-  def indent(n)
-    if n >= 0
-      gsub(/^/, ' ' * n)
-    else
-      gsub(/^ {0,#{-n}}/, "")
-    end
-  end
-  
-  def tab(n)
-    gsub(/^ */, ' ' * n)
-  end
-  
-  def tabto(n)
-    if self =~ /^( *)\S/
-      indent(n - $1.length)
-    else
-      self
-    end
-  end
-  
+class String
   def then_add(string)
     self << "\n#{string}"
   end
-  
-  def truncate(length = 30, truncate_string = "...")
-    l = length - truncate_string.length
-    self.length > length ? self[0...l] + truncate_string : self
-  end
-  
 end
-
-String.send :include, StringHelpers
